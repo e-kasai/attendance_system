155f6c5ca9b07d93669751dd1ac9ce0ec2288631
ekasai <ekasai.dev@gmail.com>
2025-10-15 13:32:14 +0900

    共通レイアウトファイル：ヘッダー部分を作成

---
+++ src/resources/views/layouts/header.blade.php
@@ -0,0 +1,32 @@
+<header class="header">
+    <div class="header__logo">
+        <img class="logo-image" src="{{ asset("img/logo.svg") }}" alt="No Image" />
+    </div>
+    @unless (request()->routeIs("login", "admin.login", "register.*", "verification.*"))
+        {{-- ログイン時のみナビとログアウト表示 --}}
+        @auth
+            <nav class="nav">
+                {{-- 管理者と一般ユーザーでナビを切り替え --}}
+                @if (auth()->user()->is_admin)
+                    <a class="nav__link" href="{{ route("admin.attendances.index") }}">勤怠一覧</a>
+                    <a class="nav__link" href="{{ route("admin.staff.index") }}">スタッフ一覧</a>
+                    <a class="nav__link" href="{{ route("admin.requests.index") }}">申請一覧</a>
+
+                    <form class="nav__logout-form" action="{{ route("logout") }}" method="POST">
+                        @csrf
+                        <button class="nav__logout-btn" type="submit">ログアウト</button>
+                    </form>
+                @else
+                    {{-- スタッフ用ナビ --}}
+                    <a class="nav__link" href="{{ route("attendance.show") }}">勤怠</a>
+                    <a class="nav__link" href="{{ route("attendances.index") }}">勤怠一覧</a>
+                    <a class="nav__link" href="{{ route("requests.index") }}">申請</a>
+                    <form class="nav__logout-form" action="{{ route("logout") }}" method="POST">
+                        @csrf
+                        <button class="nav__logout-btn" type="submit">ログアウト</button>
+                    </form>
+                @endif
+            </nav>
+        @endauth
+    @endunless
+</header>
