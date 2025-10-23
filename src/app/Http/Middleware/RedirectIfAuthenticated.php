f2280c4e01c9065c03a900505c30908ad1b5f2b6
ekasai <ekasai.dev@gmail.com>
2025-10-23 12:03:38 +0900

    ログイン済みユーザーがログインページアクセス時の遷移先をroleごとに分岐

--- src/app/Http/Middleware/RedirectIfAuthenticated.php
+++ src/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -19,12 +19,15 @@ class RedirectIfAuthenticated
      */
     public function handle(Request $request, Closure $next, ...$guards)
     {
-        $guards = empty($guards) ? [null] : $guards;
+        if (Auth::check()) {
+            $user = Auth::user();
 
-        foreach ($guards as $guard) {
-            if (Auth::guard($guard)->check()) {
-                return redirect(RouteServiceProvider::HOME);
+            // ロールごとに分岐
+            if ($user->role === 'admin') {
+                return redirect('/admin/attendance/list');
             }
+
+            return redirect('/attendance');
         }
 
         return $next($request);
