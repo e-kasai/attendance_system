62795efccc0931a9dc4c59821cc892b3ce3efd59
ekasai <ekasai.dev@gmail.com>
2025-11-13 15:18:36 +0900

    CSSに合わせて日付ナビ部のクラス名を変更、アイコンイメージを追加

--- src/resources/views/admin/daily_attendances.blade.php
+++ src/resources/views/admin/daily_attendances.blade.php
@@ -1,27 +1,31 @@
 @extends("layouts.app")
 
 @push("styles")
-    <link rel="stylesheet" href="{{ asset("css/components/list.css") }}" />
+    <link rel="stylesheet" href="{{ asset("css/components/index.css") }}" />
 @endpush
 
 @section("content")
     <x-index.container :title="__('' . \Carbon\Carbon::parse($selectedDate)->isoFormat('YYYY年M月D日') . 'の勤怠')">
-        {{-- 日付ナビ部分（monthNavスロットを利用） --}}
-        <x-slot name="monthNav">
+        {{-- 日付ナビ部分（calendarNavスロットを利用） --}}
+        <x-slot name="calendarNav">
             <form class="date-select" method="GET" action="{{ route("admin.attendances.index") }}">
                 {{-- 前日 --}}
-                <button class="date-select__button" type="submit" name="target_date" value="{{ $prevDate }}">← 前日</button>
+                <button class="date-select__button" type="submit" name="target_date" value="{{ $prevDate }}">
+                    <img class="select__arrow-icon" src="{{ asset("img/arrow_icon.png") }}" alt="logo" />
+                    前月
+                </button>
 
-                {{-- 日付表示（クリックで今日＝クエリ無し） --}}
+                {{-- 選択した日付 --}}
                 <div class="date-select__display">
-                    <a href="{{ route("admin.attendances.index") }}" title="今日に戻る">
-                        <i class="fa-solid fa-calendar"></i>
-                        {{ \Carbon\Carbon::parse($selectedDate)->format("Y/m/d") }}
-                    </a>
+                    <img class="select__calendar-icon" src="{{ asset("img/calendar_icon.png") }}" alt="logo" />
+                    <span class="month-select__text">{{ \Carbon\Carbon::parse($selectedDate)->format("Y/m/d") }}</span>
                 </div>
 
                 {{-- 翌日 --}}
-                <button class="date-select__button" type="submit" name="target_date" value="{{ $nextDate }}">翌日 →</button>
+                <button class="date-select__button" type="submit" name="target_date" value="{{ $nextDate }}">
+                    翌月
+                    <img class="select__arrow-icon next" src="{{ asset("img/arrow_icon.png") }}" alt="logo" />
+                </button>
             </form>
         </x-slot>
 
@@ -30,11 +34,15 @@
             <x-index.table :headers="['名前', '出勤', '退勤', '休憩', '合計', '詳細']">
                 @foreach ($attendances as $attendance)
                     <tr>
-                        <td>{{ $attendance->user->name }}</td>
-                        <td>{{ $attendance->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format("H:i") : "" }}</td>
-                        <td>{{ $attendance->clock_out ? \Carbon\Carbon::parse($attendance->clock_out)->format("H:i") : "" }}</td>
+                        <td class="index-table__cell">{{ $attendance->user->name }}</td>
+                        <td class="index-table__cell">
+                            {{ $attendance->clock_in ? \Carbon\Carbon::parse($attendance->clock_in)->format("H:i") : "" }}
+                        </td>
+                        <td class="index-table__cell">
+                            {{ $attendance->clock_out ? \Carbon\Carbon::parse($attendance->clock_out)->format("H:i") : "" }}
+                        </td>
                         {{-- 合計（休憩） --}}
-                        <td>
+                        <td class="index-table__cell">
                             @if (! is_null($attendance->break_time))
                                 @php
                                     $hours = floor($attendance->break_time / 60);
@@ -46,7 +54,7 @@
                         </td>
 
                         {{-- 合計（勤務時間） --}}
-                        <td>
+                        <td class="index-table__cell">
                             @if (! is_null($attendance->work_time))
                                 @php
                                     $hours = floor($attendance->work_time / 60);
@@ -56,8 +64,8 @@
                                 {{ sprintf("%02d:%02d", $hours, $minutes) }}
                             @endif
                         </td>
-                        <td>
-                            <a href="{{ route("admin.attendance.detail", $attendance->id) }}">詳細</a>
+                        <td class="index-table__cell">
+                            <a class="index-table__link" href="{{ route("admin.attendance.detail", $attendance->id) }}">詳細</a>
                         </td>
                     </tr>
                 @endforeach
