62ffccf5c2b5bf820643e9cc510b325bc82e3ba2
ekasai <ekasai.dev@gmail.com>
2025-11-15 13:35:11 +0900

    不要コメントアウト削除

--- src/routes/web.php
+++ src/routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use App\Http\Controllers\Auth\RegisterController;
 use App\Http\Controllers\Staff\StaffAttendanceController;
